c7cfa5cfd3ecb0f3bf908283159383fd03af42dc
expl0r3rgu1 <salvaddimando@gmail.com>
2023-12-20 19:01:15 +0100

    Add sequential k-means clustering implementation and compare performance with parallel version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from multiprocessing import Pool
+import time
 
 def assign_to_centroids(points, centroids):
     distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
@@ -22,6 +23,16 @@ def kmeans_parallel(points, n_clusters, n_processes, max_iter=100):
             initial_centroids = new_centroids
     return initial_centroids, closest
 
+def kmeans_sequential(points, n_clusters, max_iter=100):
+    initial_centroids = points[np.random.choice(len(points), n_clusters, replace=False)]
+    for _ in range(max_iter):
+        closest = assign_to_centroids(points, initial_centroids)
+        new_centroids = compute_centroids(points, closest, n_clusters)
+        if np.all(initial_centroids == new_centroids):
+            break
+        initial_centroids = new_centroids
+    return initial_centroids, closest
+
 # Generate sample data
 n_samples = 1000
 n_clusters = 4
@@ -29,9 +40,34 @@ n_processes = 2
 X, _ = make_blobs(n_samples=n_samples, centers=n_clusters, random_state=0, cluster_std=0.60)
 
 # Perform k-means clustering
-centroids, labels = kmeans_parallel(X, n_clusters, n_processes)
 
-# Plot the points and the cluster centers
-plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
-plt.scatter(centroids[:, 0], centroids[:, 1], c='black', s=200, alpha=0.5)
+# Time the parallel version
+start_par = time.time()
+centroids_par, labels_par = kmeans_parallel(X, n_clusters, n_processes)
+end_par = time.time()
+print(f"Parallel version took {end_par - start_par} seconds")
+
+# Time the sequential version
+start_seq = time.time()
+centroids_seq, labels_seq = kmeans_sequential(X, n_clusters)
+end_seq = time.time()
+print(f"Sequential version took {end_seq - start_seq} seconds")
+
+# Create a figure with 2 subplots
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+# Plot the points and the cluster centers (parallel)
+axs[0].scatter(X[:, 0], X[:, 1], c=labels_par, s=50, cmap='viridis')
+axs[0].scatter(centroids_par[:, 0], centroids_par[:, 1], c='black', s=200, alpha=0.5)
+axs[0].set_title('Parallel')
+
+# Plot the points and the cluster centers (sequential)
+axs[1].scatter(X[:, 0], X[:, 1], c=labels_seq, s=50, cmap='viridis')
+axs[1].scatter(centroids_seq[:, 0], centroids_seq[:, 1], c='black', s=200, alpha=0.5)
+axs[1].set_title('Sequential')
+
+# Save the figure
+fig.savefig('results/plot.png')
+
+# Show the plot
 plt.show()
